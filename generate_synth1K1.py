a4e84873d2a078844493850a8d01d12082d80f56
Jordie Shier <jordieshier@gmail.com>
2021-04-23 23:49:12 -0700

    Update mp3 to VBR mp3

--- generate_synth1K1.py
+++ generate_synth1K1.py
@@ -7,53 +7,84 @@ Script for generating synth1K1
 import os
 import sys
 import argparse
+from subprocess import DEVNULL, check_call
 
-import numpy as np
 import torch
 from tqdm import tqdm
-from pydub import AudioSegment
+import soundfile as sf
 
 from torchsynth.synth import Voice
+import torchsynth.util as util
 
 
-def generate_synth1k1(output):
+def generate_synth1k1(output, concat):
 
     voice = Voice()
+
+    if torch.cuda.is_available():
+        voice.to("cuda")
+        print("Running on CUDA")
 
     output_dir = os.path.abspath(output)
     if not os.path.isdir(output_dir):
         raise ValueError("outdir must be a directory")
 
+    audio_list = []
     with torch.no_grad():
         for i in tqdm(range(8)):
             audio = voice(i)
 
             # Write all the audio to disk
             for k in range(len(audio)):
-                index = voice.batch_size * i + k
-                filename = os.path.join(output_dir, f"synth1K1-{index}")
-                audio_data = audio[k].numpy().astype(np.float32)
-                audio_data *= 2147483647
-                audio_data = audio_data.astype(np.int32)
-                sound = AudioSegment(
-                    audio_data.tobytes(),
-                    frame_rate=int(voice.sample_rate.item()),
-                    sample_width=4,
-                    channels=1
-                )
-                sound.export(f"{filename}.mp3", format="mp3")
+                audio_list.append(audio[k])
+
+                # Save individual audio files if not concatenating
+                if not concat:
+                    index = voice.batch_size * i + k
+                    filename = os.path.join(output_dir, f"synth1K1-{index}")
+
+                    # Write WAV file
+                    wav_file = f"{filename}.wav"
+                    sf.write(
+                        wav_file,
+                        audio[k].cpu().numpy(),
+                        voice.sample_rate.cpu().int().item(),
+                    )
+
+                    # Convert to mp3 then delete WAV
+                    check_call(
+                        ["lame", "-V0", f"{wav_file}", f"{filename}.mp3"],
+                        stdout=DEVNULL,
+                        stderr=DEVNULL,
+                    )
+                    os.remove(wav_file)
+
+    if concat:
+        pad = int(voice.sample_rate.cpu().item() * 0.1)
+        audio_list = [
+            torch.nn.functional.pad(s, (0, pad)) for s in audio_list
+        ]
+        audio = torch.cat(audio_list)
+        filename = os.path.join(output_dir, "synth1K1.wav")
+        sf.write(filename, audio.cpu().numpy(), voice.sample_rate.cpu().int())
 
 
 def main(arguments):
 
     parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('outdir', help="Output directory for audio files", type=str)
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument("output", help="Output directory", type=str)
+    parser.add_argument(
+        "--concat",
+        default=False,
+        action="store_true",
+        help="Store as single file concatenated together",
+    )
     args = parser.parse_args(arguments)
 
-    generate_synth1k1(args.outdir)
+    generate_synth1k1(args.output, args.concat)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
